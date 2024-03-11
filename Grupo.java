95a7947152d1428c6daf19175bbba561449eeeab
KatheArango <131723357+KatheArango@users.noreply.github.com>
2024-03-11 00:45:11 -0500

    Add files via upload

---
+++ Grupo.java
@@ -0,0 +1,17 @@
+
+package juegocartas2;
+
+public enum Grupo {
+    VACIO,
+    NON,
+    PAR,
+    TERNA,
+    CUARTA,
+    QUINTA,
+    SEXTA,
+    SEPTIMA,
+    OCTAVA,
+    NOVENA,
+    DECIMA
+    
+}
---
+++ JuegoCartas2.java
@@ -0,0 +1,10 @@
+
+package juegocartas2;
+
+public class JuegoCartas2 {
+
+    public static void main(String[] args) {
+        new FrmJuego().setVisible(true);
+    }
+    
+}
